2eaa7f8d715fed186b12912396a7821c328e2a49
lambdart <lambdart@protonmail.com>
2021-06-22 19:14:07 -0300

    Cache eldoc metadata

--- iclj-eldoc.el
+++ iclj-eldoc.el
@@ -39,7 +39,15 @@
 (require 'iclj-comint)
 (require 'iclj-op-table)
 
-;; from elisp-mode.el.gz
+(defvar iclj-eldoc-thing ""
+  "Cache eldoc thing at point.")
+
+(defvar iclj-eldoc-meta-data nil
+  "Cache eldoc meta data information.")
+
+(defvar iclj-eldoc-callback nil
+  "Cache eldoc callback function.")
+
 (defun iclj-eldoc-beginning-of-sexp ()
   "Move to the beginning of current sexp.
 Return the number of nested sexp the point was over or after."
@@ -70,15 +78,10 @@ Return the number of nested sexp the point was over or after."
     ;; goes to the beginning of the s-expression
     (iclj-eldoc-beginning-of-sexp)
     ;; don't do anything if current word is inside a string, vector,
-    ;; hash or set literal. (with the compliments of inf-clojure :))
-    (if (member (or (char-after (1- (point))) 0) '(?\" ?\{ ?\[)) ""
+    ;; hash or set literal
+    (if (member (or (char-after (1- (point))) 0) '(?\" ?\{ ?\[))
+        ""
       (or (thing-at-point 'symbol) ""))))
-
-(defvar iclj-eldoc-callback nil
-  "Eldoc cached callback function.")
-
-(defvar iclj-eldoc-doc-string nil
-  "Cache previous doc string.")
 
 (defun iclj-eldoc-clean (output-buffer)
   "Clean internal variables and operation OUTPUT-BUFFER."
@@ -87,6 +90,27 @@ Return the number of nested sexp the point was over or after."
   ;; kill output buffer
   (kill-buffer output-buffer))
 
+(defun iclj-eldoc-parse-docstring (docstring)
+  "Parse DOCSTRING output documentation string."
+  (if (not docstring)
+      ""
+    (let ((docstring (replace-regexp-in-string "\n" "" docstring)))
+      (replace-regexp-in-string "  " " " docstring))))
+
+(defun iclj-eldoc-display-docstring (meta-data callback)
+  "Display eldoc documentation string.
+Parse the documentation string and its META-DATA,
+using the CALLBACK function."
+  ;; check and invoke callback function
+  (let ((fnsym (car meta-data))
+        (args (cadr meta-data))
+        (docstring (caddr meta-data)))
+    (when (functionp callback)
+      (funcall callback
+               (concat args " " (iclj-eldoc-parse-docstring docstring))
+               :thing fnsym
+               :face font-lock-function-name-face))))
+
 (defun iclj-eldoc-function (callback &rest _ignored)
   "Clojure documentation function.
 Each hook function is called with at least one argument CALLBACK,
@@ -94,24 +118,23 @@ a function, and decides whether to display a doc short string
 about the context around point."
   (let ((thing (iclj-eldoc-fnsym)))
     (unless (string= thing "")
-      ;; call the eldoc operations
-      (when (process-live-p (get-buffer-process iclj-comint-buffer))
-        (iclj-op-eldoc thing)
-        ;; cache eldoc callback
-        (setq iclj-eldoc-callback callback)
-        ;; - If the computation of said doc string (or the decision whether
-        ;; there is one at all) is expensive or can't be performed
-        ;; directly, the hook function should return a non-nil, non-string
-        ;; value and arrange for CALLBACK to be called at a later time,
-        ;; using asynchronous processes or other asynchronous mechanisms.
-        0))))
-
-(defun iclj-eldoc-parse-doc-string (doc)
-  "Parse DOC string.
-Meta output documentation field."
-  (if (not doc) ""
-    (let ((docstring (replace-regexp-in-string "\n" "" doc)))
-      (replace-regexp-in-string "  " " " docstring))))
+      ;; verify if we have the same thing
+      (if (string= thing iclj-eldoc-thing)
+          (iclj-eldoc-display-docstring iclj-eldoc-meta-data
+                                        callback)
+        ;; else: call the eldoc operations
+        (when (process-live-p (get-buffer-process iclj-comint-buffer))
+          (iclj-op-eldoc thing)
+          ;; cache eldoc callback
+          (setq iclj-eldoc-callback callback)
+          ;; cache thing
+          (setq iclj-eldoc-thing thing)
+          ;; - If the computation of said doc string (or the decision whether
+          ;; there is one at all) is expensive or can't be performed
+          ;; directly, the hook function should return a non-nil, non-string
+          ;; value and arrange for CALLBACK to be called at a later time,
+          ;; using asynchronous processes or other asynchronous mechanisms.
+          0)))))
 
 (defun iclj-eldoc-handler (_)
   "Eldoc function handler."
@@ -120,21 +143,20 @@ Meta output documentation field."
      ;; get eldoc operation output buffer
      output-buffer
      ;; parse output and display into echo area
-     (let ((meta (read output)))
-       (when (listp meta)
-         (let ((fnsym (car meta))
-               (args (cadr meta))
-               (docs (caddr meta)))
-           ;; check and invoke callback function
-           (when (functionp iclj-eldoc-callback)
-             (funcall iclj-eldoc-callback
-                      (concat args " " (iclj-eldoc-parse-doc-string docs))
-                      :thing fnsym
-                      :face font-lock-function-name-face))))))
+     (let ((meta-data (read output)))
+       (when (listp meta-data )
+         ;; cache eldoc meta-data information
+         (setq iclj-eldoc-meta-data meta-data)
+         ;; display eldoc docstring
+         (iclj-eldoc-display-docstring iclj-eldoc-meta-data
+                                       iclj-eldoc-callback))))
     ;; clean variables and kill output buffer
     (iclj-eldoc-clean output-buffer)))
 
-;;;###autoload
+(defun iclj-eldoc-enable ()
+  "Enable eldoc operation."
+  (add-hook 'eldoc-documentation-functions #'iclj-eldoc-function nil t))
+
 (defun iclj-eldoc-disable ()
   "Disable eldoc operation."
   (interactive)
