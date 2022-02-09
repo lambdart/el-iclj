2800433da4776acc41ec5b4652426dc4d6f9d4f8
lambdart <lambdart@protonmail.com>
2022-02-09 12:25:54 -0300

    Add ns list operation handler

---
+++ iclj-ns.el
@@ -0,0 +1,54 @@
+;;; iclj-ns.el --- summary -*- lexical-binding: t -*-
+;;
+;; Author: lambdart <lambdart@protonmail.com>
+;; Maintainer: lambdart
+;; Homepage: https://github.com/lambdart/iclj-ns.el
+;; Version: 0.0.1 Alpha
+;; Keywords:
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;;; MIT License
+;;
+;; Copyright (c) 2020 lambdart
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(require 'iclj-util)
+(require 'iclj-op-table)
+
+(defvar iclj-ns-cache-list '()
+  "Namespaces cached list.")
+
+(defun iclj-ns-list-handler (output-buffer _)
+  "Handler NS-LIST OUTPUT-BUFFER."
+  (let ((content (iclj-util-buffer-content output-buffer iclj-op-table-eoc)))
+    (unless (string-empty-p content)
+      ;; parse output and display into echo area
+      (setq iclj-ns-cache-list (read content)))
+    ;; clean buffer
+    (kill-buffer output-buffer)))
+
+(provide 'iclj-ns)
+
+;;; iclj-ns.el ends here
